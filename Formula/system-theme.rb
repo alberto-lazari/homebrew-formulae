9b8408b707c524876af7fa7145282c89d262e72a
Alberto Lazari <alberto.lazari@gmail.com>
2024-09-16 20:03:35 +0200

    Update shasum

--- Formula/system-theme.rb
+++ Formula/system-theme.rb
@@ -2,11 +2,11 @@ class SystemTheme < Formula
   desc "Switch between macOS light/dark themes from CLI"
   homepage "https://github.com/alberto-lazari/system-theme"
   url "https://github.com/alberto-lazari/system-theme/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "91b6580a243f503f0be34af14de3564cc793e57b02539187edef71e398b936b8"
+  sha256 "fb6bf523b7f33b96f6b88375c37182a9f8e085aa42efd3d1f479af3f51413038"
   license "GPL-3.0-or-later"
   head "https://github.com/alberto-lazari/system-theme.git", branch: "main"
 
-  depends_on :macos
+  depends_on :macos => :mojave
 
   def install
     bin.install "system-theme"
